e9c1dd15eb78c087170a9793d7a1b43fb5c914bd
kilento <kilento@163.com>
2023-07-30 00:17:33 +0800

    看门狗复位

---
+++ watchdog/1.2_watchdog_reset/main.c
@@ -0,0 +1,29 @@
+#include "key.h"
+#include "led.h"
+#include "s3c4412_interrupt.h"
+#include "stdio.h"
+#include "uart.h"
+#include "wdt.h"
+
+int main(void)
+{
+    uart_init();
+    // key_init();
+    led_int();
+    wdt_init();
+
+    printf("OK\n");
+
+    led_ctrl(0, 1);
+    led_ctrl(1, 0);
+
+    // interrupt_init();
+
+    // interrupt_vector_table();
+
+    // 等待中断程序发生
+    while (1) {
+    }
+
+    return 0;
+}
---
+++ watchdog/1.2_watchdog_reset/wdt.c
@@ -0,0 +1,24 @@
+#include "s3c4412_wdt.h"
+
+void wdt_init(void)
+{
+    /* t_watchdog = 1 / ( PCLK / (Prescaler value + 1) / Division factor ) */
+
+    WTCON &= ~(0xff << 8);
+    WTCON |= (0x63 << 8); // 设置预分频系数为 99，则预分频后的时钟为 1MHz
+
+    WTCON &= ~(0x3 << 3);
+    WTCON |= (0x3 << 3); // 设置时钟系数为 128，则最终得到的时钟周期为  T= 1 / (1MHz / 128) = 128us
+
+    // WTCON |= (1 << 2); // 使能看门狗中断
+    // WTCON &= ~(1 << 0); //< 禁止看门狗rest
+    WTCON &= ~(1 << 2); // 禁止看门狗中断
+    WTCON |= (1 << 0); // 使能看门狗reset
+
+    WTDAT = 25000; // 初此定时： 128us * 25000 = 3.2s
+    WTCNT = 25000; // 看门狗定时间隔： 128us * 25000 = 3.2s
+
+    WTCON |= (1 << 5); // 打开看门狗
+
+    return;
+}
